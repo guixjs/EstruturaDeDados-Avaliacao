00290c7f5cd3ba048f577a0a5ce8b86b7b5d4fc4
Guilherme Felix <f3lixguilherme@gmail.com>
2024-11-30 21:02:30 -0300

    Lista duplamente encadeada 100%

--- Listas/Lista Duplamente Ligada - Campeonato Xadrez/CampeonatoXadrez - DuplamenteLigada.c
+++ Listas/Lista Duplamente Ligada - Campeonato Xadrez/CampeonatoXadrez - DuplamenteLigada.c
@@ -58,23 +58,77 @@ int inserirElemento(Lista *lista,char nome[50],int idade,char sexo,int rating,do
 int inserirElementoID(Lista *lista,char nome[50],int idade,char sexo,int rating,double pontuacao,int posicao);
 int inserirElementoInicio(Lista *lista,char nome[50],int idade,char sexo,int rating,double pontuacao);
 void limparBuffer();
-//void menuGerenciar(Lista *lista);
-//void menuOpcoes(Lista *lista);
-//void menuTorneio(Lista *lista);
+void menuGerenciar(Lista *lista);
+void menuOpcoes(Lista *lista);
+void menuTorneio(Lista *lista);
 int removerElemento(Lista *lista, char nomeBusca[50]);
 int salvarArquivo(Lista *lista); 
 int tamanhoLista(Lista *lista);
 
 int main(){
-	ListaNo *competidorEncontrado;
-	setlocale(LC_ALL,"portuguese");
+	char resposta;
+	int escolha;
+
+	setlocale(LC_ALL,"portuguese");//funcao para permitir caracteres especiais
 	Lista *lista = NULL;
-	lista = criarLista();
-	inserirElemento(lista,"Magnus",23,'M',2300,0);
-	inserirElementoID(lista,"Supi",25,'M',3400,0,2);
-	inserirElementoInicio(lista,"Nakamura",25,'M',3400,0);
-	imprimirLista(lista);
-	imprimirOrdemInversa(lista);
+	
+	do{
+		system("cls");
+		printf("============CHECKMATE ORGANIZER!============\n");
+		printf("MENU PRINCIPAL\n===========================\n");
+		printf("1.Criar Torneio\n2.Gerenciar Torneio\n3.Opções da lista\n4.Excluir Lista\n0.Sair\n");
+		printf("ESCOLHA UMA OPÇÃO: ");
+		scanf("%d",&escolha);
+		limparBuffer();
+		printf("\n");
+		switch(escolha){
+			case 1:
+				if(lista ==NULL){//verifica se ja existe uma lista, caso nao exista, cria automaticamente
+					system("cls");//serve para limpar a tela 
+					printf("MENU PRINCIPAL>CRIAR TORNEIO\n===========================\n");
+					printf("Criando lista...\n");
+					lista = criarLista();
+					getch();//serve para "pausar a tela", sai ao pressionar qualquer tecla
+				}
+				menuTorneio(lista); //chama a funcao menuTorneio (passando a lista como parametro)
+				break;
+			case 2:
+				menuGerenciar(lista);	//chama a funcao menuGerenciar (passando a lista como parametro)
+				break;
+			case 3:
+				menuOpcoes(lista); //chama a funcao menuOpcoes (passando a lista como parametro)
+				break;
+			case 4://opcao para excluir a lista
+				system("cls");
+				printf("MENU PRINCIPAL>EXCLUIR LISTA\n=====================\n");
+				printf("Tem certeza que deseja excluir a lista? (S/N)\n");
+				scanf("%c",&resposta); //confirmacao se realmente quer excluir a lista
+				limparBuffer();
+				
+				if(resposta=='S'|| resposta=='s'){
+					lista = excluirLista(lista);//se digitar "S" ou "s" apaga a lista
+					if(lista==NULL){
+						printf("Lista excluída\n");//verifica se conseguiu apagar
+					}else{
+						printf("Erro ao excluir a lista");
+					}
+				}else{ //se digitar diferente de S\s ele cancela
+					printf("Exclusão da lista cancelada\n");
+				}
+				getch();
+				break;
+			case 0:
+				break;
+			default:
+				printf("POR FAVOR DIGITE UMA OPÇÃO VÁLIDA\n");
+				getch();
+				system("cls");
+				break;
+		}
+			
+	}while(escolha!=0);
+	printf("OBRIGADOR POR UTILIZAR O CHECKMATE ORGANIZER!\n"); //mensagem de encerramento
+	
 	
 	return 0;
 }
@@ -270,8 +324,16 @@ int inserirElementoID(Lista* lista, char nome[50],int idade,char sexo, int ratin
 	nova->sexo = sexo;
 	nova->rating = rating;
 	nova->pontuacao = pontuacao;
-	
-	if(posicao ==1 || lista->prim ==NULL){
+	nova->prox = NULL;
+	nova->ant = NULL;
+	if(lista->prim == NULL){
+		nova->prox = lista->prim;
+		nova->ant = NULL;
+		lista->prim = nova;
+		lista->ult = nova;
+		return 1;
+	}
+	if(posicao ==1){
 		nova->prox = lista->prim;
 		nova->ant = NULL;
 		lista->prim->ant = nova;
@@ -307,7 +369,7 @@ int inserirElementoID(Lista* lista, char nome[50],int idade,char sexo, int ratin
 	return 0;
 }
 
-int inserirElementoInicio(Lista *lista,char nome[50],int idade,char sexo,int rating,double pontuacao){
+int inserirElementoInicio(Lista *lista,char nome[50],int idade,char sexo,int rating,double pontuacao){	
 	if(lista == NULL){
 		printf("A lista não foi criada\n");
 		return 0;
@@ -317,14 +379,16 @@ int inserirElementoInicio(Lista *lista,char nome[50],int idade,char sexo,int rat
 		printf("Sem espaço\n");
 		return 0;
 	}
+	
+	lista->prim->ant = nova;
 	strcpy (nova->nome, nome);
 	nova->idade = idade;
 	nova->sexo = sexo;
 	nova->rating = rating;
 	nova->pontuacao = pontuacao;
+
 	nova->prox = lista->prim;
 	nova->ant = NULL;
-	lista->prim->ant = nova;
 	
 	lista->prim = nova;
 	return 1;
@@ -337,6 +401,307 @@ void limparBuffer(){
 	}while(c!='\n');
 }
 
+void menuGerenciar(Lista *lista){
+	int escolhaGerenciar,verificar;
+	char nomeBusca[50],nomeTemp[50], sexoTemp;
+	int idadeTemp, ratingTemp;
+	double pontuacaoTemp;
+	ListaNo *pEncontrado;
+	
+	do{
+		system("cls");
+		printf("MENU PRINCIPAL>MENU GERENCIAR TORNEIO\n=====================\n");
+		printf("1.Listar Participantes\n2.Listar Participantes na Ordem Inversa\n3.Remover Participante\n4.Buscar Participante\n5.Alterar Participante\n0.Retornar ao Menu Principal\n");
+		printf("ESCOLHA UMA OPÇÃO: ");
+		scanf("%d",&escolhaGerenciar);
+		limparBuffer();
+		printf("\n");
+		switch(escolhaGerenciar)
+		{
+			case 1:
+				system("cls");
+				printf("MENU PRINCIPAL>MENU GERENCIAR TORNEIO>LISTAR PARTICIPANTES\n=====================\n");
+				imprimirLista(lista);
+				getch();
+				break;
+			case 2:
+				system("cls");
+				printf("MENU PRINCIPAL>MENU GERENCIAR TORNEIO>LISTAR PARTICIPANTES\n=====================\n");
+				imprimirOrdemInversa(lista);
+				getch();
+				break;
+			case 3:
+				system("cls");
+				printf("MENU PRINCIPAL>MENU GERENCIAR TORNEIO>REMOVER PARTICIPANTE\n=====================\n");
+				printf("DIGITE O NOME DO PARTICIPANTE QUE DESEJA REMOVER: ");
+				fgets(nomeBusca, sizeof(nomeBusca), stdin);
+				nomeBusca[strcspn(nomeBusca, "\n")] = 0;
+				verificar=removerElemento(lista, nomeBusca);
+				if(verificar ==1){
+					printf("PARTICIPANTE REMOVIDO COM SUCESSO\n");
+					getch();
+				}else
+				{
+					printf("PARTICIPANTE NÃO ENCONTRADO\n");
+					getch();
+				}
+				break;
+			case 4:
+				system("cls");
+				printf("MENU PRINCIPAL>MENU GERENCIAR TORNEIO>BUSCAR PARTICIPANTE\n=====================\n");
+				printf("DIGITE O NOME DO PARTICIPANTE QUE DESEJA BUSCAR: ");
+				fgets(nomeBusca, sizeof(nomeBusca), stdin);
+				nomeBusca[strcspn(nomeBusca, "\n")] = 0;
+				pEncontrado = buscarElemento(lista,nomeBusca);
+				printf("\n");
+				if(pEncontrado!=NULL){
+					printf("NOME: %s\n",pEncontrado->nome);
+					printf("IDADE: %d\n",pEncontrado->idade);
+					printf("SEXO: %c\n",pEncontrado->sexo);
+					printf("RATING: %d\n",pEncontrado->rating);
+					printf("PONTUAÇÃO: %.1lf\n",pEncontrado->pontuacao);
+					printf("=====================\n");
+					getch();
+				}else{
+					printf("PARTICIPANTE NÃO ENCONTRADO\n");
+					getch();
+				}
+				break;
+			case 5:
+				system("cls");
+				printf("MENU PRINCIPAL>MENU GERENCIAR TORNEIO>ALTERAR PARTICIPANTE\n=====================\n");
+				printf("DIGITE O NOME DO PARTICIPANTE QUE DESEJA SUBSTITUIR: ");
+				fgets(nomeBusca, sizeof(nomeBusca), stdin);
+				nomeBusca[strcspn(nomeBusca, "\n")] = 0;
+				pEncontrado = buscarElemento(lista,nomeBusca);
+				
+				if(pEncontrado==NULL){
+					printf("PARTICIPANTE NÃO ENCONTRADO\n");
+					getch();
+					break;
+				}
+				printf("PARTICIPANTE ENCONTRADO\n");
+				printf("Pressione qualquer tecla para continuar\n");
+				getch();
+				system("cls");
+				printf("MENU PRINCIPAL>MENU GERENCIAR TORNEIO>ALTERAR PARTICIPANTE>NOVO CADASTRO\n=====================\n");
+				printf("DIGITE O NOME DO NOVO PARTICIPANTE: ");
+				fgets(nomeTemp, sizeof(nomeTemp), stdin);
+				nomeTemp[strcspn(nomeTemp, "\n")] = 0;
+				printf("DIGITE A IDADE DO NOVO PARTICIPANTE: ");
+				scanf("%d",&idadeTemp);
+				limparBuffer();
+				printf("DIGITE O SEXO DO NOVO PARTICIPANTE: ");
+				scanf("%c",&sexoTemp);
+				limparBuffer();
+				printf("DIGITE O RATING DO NOVO PARTICIPANTE: ");
+				scanf("%d",&ratingTemp);
+				limparBuffer();
+				printf("DIGITE A PONTUAÇÃO DO NOVO PARTICIPANTE: ");
+				scanf("%lf",&pontuacaoTemp);
+				limparBuffer();
+				verificar = atualizarElemento(lista,nomeBusca,nomeTemp,idadeTemp,sexoTemp,ratingTemp,pontuacaoTemp);
+				if(verificar == 1){
+					printf("PARTICIPANTE CADASTRADO COM SUCESSO!\n");
+				}else{
+					printf("NÃO FOI POSSÍVEL CADASTRAR O PARTICIPANTE\n");
+				}
+				getch();
+				
+				break;
+			case 0:
+				continue;
+				break;
+			default:
+				printf("POR FAVOR, DIGITE UMA OPÇÃO VÁLIDA\n");
+				getch();
+				system("cls");
+				break;
+		}	
+	}while(escolhaGerenciar!=0);
+}
+/*************************************************
+	NOME: menuOpcoes
+	PARÂMETROS: lista
+	RETORNO: void
+	DESCRIÇÃO: uma funcao que é chamada no menu principal. Imprimi o menu "Opcoes da lista"
+
+**************************************************/
+void menuOpcoes(Lista *lista){
+	int escolhaOpcoes,tamanho, verificar;
+	char mensagem[10];
+	do{
+		system("cls");
+		printf("MENU PRINCIPAL>OPÇÕES TORNEIO\n=====================\n");
+		printf("1.Salvar dados\n2.Carregar dados\n3.Tamanho da lista\n0.Retornar ao Menu Principal\n");
+		printf("ESCOLHA UMA OPÇÃO: ");
+		scanf("%d",&escolhaOpcoes);
+		limparBuffer();
+		printf("\n");
+		switch(escolhaOpcoes)
+		{
+		case 1:
+			system("cls");
+			printf("MENU PRINCIPAL>OPÇÕES TORNEIO>SALVAR DADOS\n=====================\n");
+			verificar=salvarArquivo(lista);
+			if(verificar==1){
+				printf("LISTA SALVA COM SUCESSO\n");
+				getch();
+			}
+			break;
+		case 2:
+			system("cls");
+			printf("MENU PRINCIPAL>OPÇÕES TORNEIO>CARREGAR DADOS\n=====================\n");
+			verificar = carregarArquivos(lista);
+			if(verificar==1){
+				printf("LISTA CARREGADA COM SUCESSO\n");
+			}else{
+				printf("NÃO FOI POSÍVEL CARREGAR A LISTA\n");
+			}
+			getch();
+			break;
+		case 3:
+			system("cls");
+			printf("MENU PRINCIPAL>OPÇÕES TORNEIO>TAMANO DA LISTA\n=====================\n");
+			tamanho = tamanhoLista(lista);
+			if(tamanho==1){
+				strcpy(mensagem,"competidor");
+			}else{
+				strcpy(mensagem,"competidores");
+			}
+			if(tamanho>0){
+				printf("A lista possui %d %s",tamanho,mensagem);	
+			}else{
+				printf("A lista não possui competidores\n");
+			}
+			getch();
+			break;
+		case 0:
+			continue;
+			break;
+		default:
+			printf("POR FAVOR, DIGITE UMA OPÇÃO VÁLIDA\n");
+			getch();
+			system("cls");
+		}
+	}while(escolhaOpcoes!=0);
+}
+/*************************************************
+	NOME: menuTorneio
+	PARÂMETROS: lista
+	RETORNO: void
+	DESCRIÇÃO: uma funcao que é chamada no menu principal. Imprimi o menu "Criar torneio"
+
+**************************************************/
+void menuTorneio(Lista *lista){
+	int escolhaTorneio;
+	char nomeTemp [50],sexoTemp;
+	int idadeTemp,ratingTemp, verificar, posicao,escolhaCadastro;
+	double pontuacaoTemp;
+	do{
+		system("cls");
+		printf("MENU PRINCIPAL>CRIAR TORNEIO\n=====================\n");
+		printf("1.Cadastrar Participante\n2.Gerar chaveamento\n0.Retornar ao Menu Principal\n");
+		printf("ESCOLHA UMA OPÇÃO: ");
+		scanf("%d",&escolhaTorneio);
+		limparBuffer();
+		printf("\n");
+		switch(escolhaTorneio){
+			case 1:
+				system("cls");
+				printf("MENU PRINCIPAL>CRIAR TORNEIO>CADASTRAR PARTICIPANTE\n=====================\n");
+				printf("1.Cadastrar no final\n2.Cadastrar no início\n3.Cadastrar em uma posição específica\n0.Retornar ao Menu Criar Torneio\n");
+				printf("ESCOLHA UMA OPÇÃO: ");
+				scanf("%d",&escolhaCadastro);
+				limparBuffer();
+				printf("\n");
+				if(escolhaCadastro>3 || escolhaCadastro<0){
+					printf("OPÇÃO INVÁLIDA, CADASTRO CANCELADO\n");
+					getch();
+					break;
+				}
+				if(escolhaCadastro==0){
+					continue;
+					break;
+				}
+				system("cls");
+				
+				if(escolhaCadastro==1){
+					printf("MENU PRINCIPAL>CRIAR TORNEIO>CADASTRAR PARTICIPANTE>CADASTRAR NO FINAL\n=====================\n");
+				}else if(escolhaCadastro==2){
+					printf("MENU PRINCIPAL>CRIAR TORNEIO>CADASTRAR PARTICIPANTE>CADASTRAR NO INÍCIO\n=====================\n");
+				}
+				else if(escolhaCadastro==3){
+					printf("MENU PRINCIPAL>CRIAR TORNEIO>CADASTRAR PARTICIPANTE>CADASTRAR NA POSIÇÃO ESPECÍFICA\n=====================\n");
+					printf("Defina a posição que o participante será cadastrado\n");
+					scanf("%d",&posicao);
+					limparBuffer();
+				}
+				printf("DIGITE O NOME DO PARTICIPANTE: ");
+				fgets(nomeTemp, sizeof(nomeTemp), stdin);
+				nomeTemp[strcspn(nomeTemp, "\n")] = 0;
+				printf("DIGITE A IDADE DO PARTICIPANTE: ");
+				scanf("%d",&idadeTemp);
+				limparBuffer();
+				printf("DIGITE O SEXO DO PARTICIPANTE: ");
+				scanf("%c",&sexoTemp);
+				limparBuffer();
+				printf("DIGITE O RATING DO PARTICIPANTE: ");
+				scanf("%d",&ratingTemp);
+				limparBuffer();
+				printf("DIGITE A PONTUAÇÃO DO PARTICIPANTE: ");
+				scanf("%lf",&pontuacaoTemp);
+				limparBuffer();
+				switch(escolhaCadastro){
+					case 1:
+						verificar = inserirElemento(lista,nomeTemp,idadeTemp,sexoTemp,ratingTemp,pontuacaoTemp);
+						if(verificar == 1){
+							printf("PARTICIPANTE CADASTRADO COM SUCESSO!\n");
+						}else{
+							printf("NÃO FOI POSSÍVEL CADASTRAR O PARTICIPANTE\n");
+						}
+						getch();
+						system("cls");
+						break;
+					case 2:
+						verificar = inserirElementoInicio(lista,nomeTemp,idadeTemp,sexoTemp,ratingTemp,pontuacaoTemp);
+						if(verificar == 1){
+							printf("PARTICIPANTE CADASTRADO COM SUCESSO!\n");
+						}else{
+							printf("NÃO FOI POSSÍVEL CADASTRAR O PARTICIPANTE\n");
+						}
+						getch();
+						system("cls");
+						break;
+					case 3:
+						verificar = inserirElementoID(lista,nomeTemp,idadeTemp,sexoTemp,ratingTemp,pontuacaoTemp,posicao);
+						if(verificar == 1){
+							printf("PARTICIPANTE CADASTRADO COM SUCESSO!\n");
+						}else{
+							printf("NÃO FOI POSSÍVEL CADASTRAR O PARTICIPANTE\n");
+						}
+						getch();
+						system("cls");
+						break;
+					}
+				break;	
+			case 2:
+				system("cls");
+				printf("MENU PRINCIPAL>GERAR CHAVEAMENTO\n=====================\n");
+				printf("FUNÇÃO EM DESENVOLVIMENTO\n");
+				getch();
+				break;
+			case 0:
+				continue;
+				break;
+			default:
+				printf("POR FAVOR, DIGITE UMA OPÇÃO VÁLIDA\n");
+				getch();
+				system("cls");
+				break;			
+		}
+	}while(escolhaTorneio!=0);
+}
 
 
 int removerElemento(Lista *lista, char nomeBusca[50]){
